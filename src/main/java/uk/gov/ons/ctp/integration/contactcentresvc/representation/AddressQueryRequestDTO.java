d56bcd8ad3ed6ac5b63c81322ae1fb12a32e46aa
Rayson Viswas <rayson.viswas@live.co.uk>
2021-03-03 11:03:55 +0000

    mvn fmt:format

--- src/main/java/uk/gov/ons/ctp/integration/contactcentresvc/representation/AddressQueryRequestDTO.java
+++ src/main/java/uk/gov/ons/ctp/integration/contactcentresvc/representation/AddressQueryRequestDTO.java
@@ -4,10 +4,9 @@ import javax.validation.constraints.Max;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Pattern;
-import uk.gov.ons.ctp.integration.contactcentresvc.Constants;
-
 import lombok.Data;
 import lombok.ToString;
+import uk.gov.ons.ctp.integration.contactcentresvc.Constants;
 
 @Data
 @ToString
